48c5964d17d930bcff9d0d4619564f31060107ae
Jeremy Utley <jutley@utleycomputerconsulting.com>
2021-09-28 15:47:46 -0500

    various fixes

--- roles/restic/files/backup_script.sh
+++ roles/restic/files/backup_script.sh
@@ -7,12 +7,11 @@
 export RESTIC_REPOSITORY="rest:http://172.17.20.13:8080/"
 export RESTIC_PASSWORD="2Az2inKeFSufBe8zmTGXtGYvkojO2y6P"
 
-startTime = `date +%Y-%m-%d %H:%M`
+startTime=`date +%Y-%m-%d\ %H:%M`
 /usr/bin/echo "Backups starting at:  ${startTime}" > /var/log/backup_script.log
 
 #Run restic to perform the backup
 /usr/local/bin/restic --exclude-file=/jobs/backup_excludes backup / >> /var/log/backup_script.log
 
-endTime = `date +%Y-%m-%d %H:%M`
-
+endTime=`date +%Y-%m-%d\ %H:%M`
 /usr/bin/echo "Backups ending at: ${endTime}" >> /var/log/backup_script.log
